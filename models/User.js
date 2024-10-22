61f96307cf4173af5eee92c3aafab4dc309287c2
vishalr0105 <vishalr0105@gmail.com>
2024-10-22 17:51:45 +0530

    change type of role

--- models/User.js
+++ models/User.js
@@ -36,11 +36,10 @@ const timesheetSchema = new mongoose.Schema({
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   usePass: { type: String, required: true },
-  role: { type: String, enum: ['admin', 'user'], default: 'user' },
+  role: { type: [String], enum: ['admin', 'kds'], default: ['kds']},
   timesheet: [timesheetSchema],
 });
 
 const User = mongoose.model('User', userSchema);
 
 export default User;
-
